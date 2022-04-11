efd5365194803fa2b38f2d6632e0dfb833abdb85
gunaya <gunnayaa@gmail.com>
2022-04-11 10:06:15 +0800

    init

---
+++ node-admin/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+	cors({
+		origin: ["http://localhost:3000"],
+	})
+);
+
+routes(app);
+
+app.listen(8002, () => {
+	console.log("listening to port 8002");
+});
